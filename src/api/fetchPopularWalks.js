00f8385b53ad8b1426aea0cf221bb8329f305b88
Elliot Dunk <elliot@elliots-imac.lan>
2020-07-01 08:34:23 +0100

    Deployed API Gateway URL Updates

--- src/api/fetchPopularWalks.js
+++ src/api/fetchPopularWalks.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 
-export default function fetchPopularWalks(
-  limit = 8,
-  latitude = 0.0969321,
-  longitude = 50.963583
-) {
+export default function fetchPopularWalks(limit = 8, latitude = 0.0969321, longitude = 50.963583) {
+  console.log(process.env.API_GATEWAY_URL);
   return new Promise((resolve, reject) => {
     axios
-      .get(
-        `http://192.168.1.149:5000/walks/search?lon=${longitude}&lat=${latitude}&limit=${limit}`
-      )
+      .get(`https://doghubgateway.herokuapp.com/walks/search?lon=${longitude}&lat=${latitude}&limit=${limit}`)
       .then((response) => {
         resolve(response.data);
       })
--- src/api/fetchWalksLocation.js
+++ src/api/fetchWalksLocation.js
@@ -1,16 +1,9 @@
 import axios from "axios";
 
-export default function fetchWalksLocation(
-  location,
-  minDistance = 0,
-  maxDistance = 80000,
-  limit = 0
-) {
+export default function fetchWalksLocation(location, minDistance = 0, maxDistance = 80000, limit = 0) {
   return new Promise((resolve, reject) => {
     axios
-      .get(
-        `http://192.168.1.149:5000/walks/search?location=${location}&minDist=${minDistance}&maxDist=${maxDistance}&limit=${limit}`
-      )
+      .get(`https://doghubgateway.herokuapp.com/walks/search?location=${location}&minDist=${minDistance}&maxDist=${maxDistance}&limit=${limit}`)
       .then((response) => {
         resolve(response.data);
       })
