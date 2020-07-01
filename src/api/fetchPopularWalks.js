a54e74e438f98f3abd6fb0cc552e3a38e61eacc9
Elliot Dunk <elliot@elliots-imac.lan>
2020-07-01 08:36:00 +0100

    Console Log Clean Up

--- src/api/fetchPopularWalks.js
+++ src/api/fetchPopularWalks.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default function fetchPopularWalks(limit = 8, latitude = 0.0969321, longitude = 50.963583) {
-  console.log(process.env.API_GATEWAY_URL);
   return new Promise((resolve, reject) => {
     axios
       .get(`https://doghubgateway.herokuapp.com/walks/search?lon=${longitude}&lat=${latitude}&limit=${limit}`)
