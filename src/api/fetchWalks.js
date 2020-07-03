f0321c21c30bb0c4e341e61188ce41ebd094e777
Elliot Dunk <elliot@elliots-mbp.lan>
2020-07-03 11:30:19 +0100

    API URL Updates

--- src/api/fetchWalks.js
+++ src/api/fetchWalks.js
@@ -4,9 +4,19 @@ const API_URL = "http://doghubgateway.herokuapp.com/walks";
 
 export default class FetchWalks {
   static location({ location, latitude, longitude, minDistance = 0, maxDistance = 80000, limit = 0 }) {
+    const locationQuery = typeof location === "undefined" ? "" : "&location=" + location;
+    const latQuery = typeof latitude === "undefined" ? "" : "&lat=" + latitude;
+    const lngQuery = typeof longitude === "undefined" ? "" : "&lng=" + longitude;
+    const minDistQuery = typeof minDistance === "undefined" ? "" : "&minDist=" + minDistance;
+    const maxDistQuery = typeof maxDistance === "undefined" ? "" : "&maxDist=" + maxDistance;
+    const limitQuery = typeof limit === "undefined" ? "" : "&limit=" + limit;
+
+    console.log(typeof latitude);
+    console.log(`${API_URL}/search?${locationQuery}${latQuery}${lngQuery}${minDistQuery}${maxDistQuery}${limitQuery}`);
+
     return new Promise((resolve, reject) => {
       axios
-        .get(`${API_URL}/search?location=${location}&minDist=${minDistance}&maxDist=${maxDistance}&limit=${limit}`)
+        .get(`${API_URL}/search?location=${locationQuery}${latQuery}${lngQuery}${minDistQuery}${maxDistQuery}${limitQuery}`)
         .then((response) => {
           if (response.status >= 200 && response.status < 300) {
             resolve(response.data);
